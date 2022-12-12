59d644c78f076fa8f92758e657e80470b8f84564
Adilet <adiletashuraliev1@gmail.com>
2022-12-12 10:30:06 +0600

    .

---
+++ main/filters.py
@@ -0,0 +1,12 @@
+from django_filters.rest_framework import FilterSet
+import django_filters
+
+from .models import Product
+
+class ProductFilter(FilterSet):
+    category_title = django_filters.CharFilter(field_name='caregory__title')
+    category_id = django_filters.NumberFilter(field_name='category')
+
+    class Meta:
+        model = Product
+        fields = ['category_title', 'category_id', 'status']
---
+++ main/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Category, Product
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['category'] = CategorySerializer(instance.category).data
+        return rep 
